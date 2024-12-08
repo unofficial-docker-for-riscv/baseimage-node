a089f141474c765c966a8818d1c7c426f86e955b
ngc7331 <ngc7331@outlook.com>
2024-12-08 15:49:14 +0800

    feat: update to alpine3.21 to support node22/23

--- build-alpine.sh
+++ build-alpine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ALPINE_VERSION=3.20
+ALPINE_VERSION=${ALPINE_VERSION:-3.21}
+NODE_USE_CURRENT=${NODE_USE_CURRENT:-false}
 
 get_version() {
   local package=$1
@@ -10,7 +11,13 @@ get_version() {
   export ${prefix}_RELEASE=$(echo "${apkbuild}" | grep pkgrel= | cut -d= -f2)
 }
 
-get_version "nodejs" "NODE"
+if [[ "${NODE_USE_CURRENT}" == "true" ]]; then
+  get_version "nodejs-current" "NODE" "community"
+  export NODE="nodejs-current"
+else
+  get_version "nodejs" "NODE"
+  export NODE="nodejs"
+fi
 echo "node.js: ${NODE_VERSION}-r${NODE_RELEASE}"
 NODE_VERSION_MAJOR=$(echo "${NODE_VERSION}" | cut -d. -f1)
 NODE_VERSION_MINOR=$(echo "${NODE_VERSION}" | cut -d. -f2)
@@ -22,10 +29,19 @@ REPOS=(${REPOS:-ngc7331/riscv-baseimage-node})
 TAGS=()
 for repo in ${REPOS[@]}; do
   for suffix in "" "${ALPINE_VERSION}"; do
-    for version in ${NODE_VERSION_MAJOR} ${NODE_VERSION_MAJOR}.${NODE_VERSION_MINOR} ${NODE_VERSION} "current"; do
+    VERSIONS=("${NODE_VERSION_MAJOR}" "${NODE_VERSION_MAJOR}.${NODE_VERSION_MINOR}" "${NODE_VERSION}")
+    if [[ "${NODE_VERSION_MAJOR}" == "23" ]]; then
+      VERSIONS+=("current")
+      TAGS+=("-t ${repo}:alpine${suffix}")
+    elif [[ "${NODE_VERSION_MAJOR}" == "22" ]]; then
+      VERSIONS+=("lts" "jod")
+    elif [[ "${NODE_VERSION_MAJOR}" == "20" ]]; then
+      VERSIONS+=("iron")
+    fi
+
+    for version in ${VERSIONS[@]}; do
       TAGS+=("-t ${repo}:${version}-alpine${suffix}")
     done
-    TAGS+=("-t ${repo}:alpine${suffix}")
   done
 done
 
@@ -35,6 +51,7 @@ docker buildx build \
   ${TAGS[@]} \
   --push \
   --build-arg ALPINE_VERSION=${ALPINE_VERSION} \
+  --build-arg NODE=${NODE} \
   --build-arg NODE_VERSION=${NODE_VERSION} \
   --build-arg NODE_RELEASE=${NODE_RELEASE} \
   --build-arg YARN_VERSION=${YARN_VERSION} \
